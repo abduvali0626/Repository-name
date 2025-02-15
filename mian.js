b2a6c261e9ccb83caa5a34eb76061d58034f9d36
abduvali0626 <umarrrrrrrrrrrrrrr63@gmail.com>
2025-02-15 12:20:09 +0500

    Add files via upload

---
+++ mian.js
@@ -0,0 +1,23 @@
+let text = document.getElementById("pii");
+let happy = document.getElementById("pii2");
+
+function press1() {
+    pii.textContent = "$800";
+    pii2.textContent = "o a gun"
+}
+
+function press2() {
+    pii.textContent = "$700";
+    pii2.textContent = "Do you want a candy?"
+}
+
+
+function press3() {
+    pii.textContent = "$600";
+    pii2.textContent = "Cookie is not bicuit"
+}
+
+function press4() {
+    pii.textContent = "500";
+    pii2.textContent = "Do you want a map of me?"
+}
